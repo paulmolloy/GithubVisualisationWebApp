c83828692909f01ece7bc01923f6ab6b5eb5d5bb
paulmolloy <molloyp1@tcd.ie>
2017-12-13 14:33:38 +0000

    Add sqlite database

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -101,6 +101,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+#STATICFILES_DIRS = [
+        #os.path.join(BASE_DIR, "..", "static"), 
+#    ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
--- personal_website/models.py
+++ personal_website/models.py
@@ -3,3 +3,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Organization(models.Model):
+    organization_name = models.CharField(max_length=200)
+
+class Repository(models.Model):
+    repo_id = models.IntegerField()
+    repo_name = models.CharField(max_length=200)
+    organization = models.ForeignKey(Organization)
+
+class Commit(models.Model):
+    commit_id = models.IntegerField();
+    title = models.CharField(max_length=200)
+    author =  models.CharField(max_length=200) 
+    repository = models.ForeignKey(Repository)
+
+
