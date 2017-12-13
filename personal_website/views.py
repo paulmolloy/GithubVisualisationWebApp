124d0824ecfc6636a16b242ba176579c74c7554c
paulmolloy <molloyp1@tcd.ie>
2017-12-13 14:31:07 +0000

    Fetch all google repositories.

--- personal_website/views.py
+++ personal_website/views.py
@@ -8,13 +8,13 @@ from github import Github
 def index(request):
         return HttpResponse("Hello, World!")
 
-GITHUB_TOKEN = "key"
+GITHUB_TOKEN = "70c438ead2254788400a09559d93855961e3a80e"
 def github(request):
-        g = Github("paulmolloy", GITHUB_TOKEN)
-        repos = [];
-        for repo in g.get_user().get_repos():
-            repos.append(repo.name)
-            
+        #g = Github("paulmolloy", GITHUB_TOKEN)
+        #repos = [];
+        #for repo in g.get_user().get_repos():
+        #    repos.append(repo.name)
+        repos = list_org_repos('Google')    
         template = loader.get_template('personal_website/index.html')
         context = {
                 'repos': repos
@@ -31,4 +31,16 @@ def visualisation(request):
                 }
         return HttpResponse(template.render(context, request))
     
-    
+
+
+def list_org_repos(name):
+    repos = [];
+    g = Github('paulmolloy', GITHUB_TOKEN)
+    print ("got token")
+    org = g.get_organization(name)
+    print ("got org")
+    for repo in org.get_repos():
+        print ('appending' + repo.name)
+        repos.append(repo.name)
+
+    return repos
